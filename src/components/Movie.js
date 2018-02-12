a6823a76c49916b519ad06c5d72b5c78369b9907
Mario Aguiar <mario@10up.com>
2018-02-12 16:03:40 -0600

    Add poster

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import Poster from './Poster';
 
 class Movie extends Component {
 	render() {
@@ -6,7 +7,7 @@ class Movie extends Component {
 			<div className="movie">
 				<h2>{ this.props.meta.title }</h2>
 				<div>
-					<img width="200" src={ this.props.meta.poster } />
+					<Poster path={ this.props.meta.poster_path } />
 				</div>
 				<p>({ this.props.meta.release_date })</p>
 				<p>{ this.props.meta.overview }</p>
