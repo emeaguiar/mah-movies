85f6b907ccf08ef4ac0f69e524e0376df6adbdeb
Mario Aguiar <mario@10up.com>
2018-02-12 16:21:36 -0600

    Move loading function to Movies component

--- src/components/App.js
+++ src/components/App.js
@@ -9,24 +9,11 @@ class App extends Component {
 
     this.state = {
       conf: []
-    };
-
-    this.loadAdditionalMovies = this.loadAdditionalMovies.bind( this );
-  }
-
-  componentWillMount() {
-    fetch( 'https://api.themoviedb.org/3/configuration?api_key=985e24fe0adef6dbea8259e606b71a37' )
-      .then( results => {
-        return results.json();
-      } )
-      .then( data => {
-        this.setState( { conf: data } );
-      } );
+	};
   }
 
   render() {
     return (
-
       <div className="App">
         <Header text="Mario's Movie Mojo app!" />
         <p className="App-intro">
@@ -38,13 +25,6 @@ class App extends Component {
         </div>
       </div>
     );
-  }
-
-  loadAdditionalMovies() {
-    /* var currentMovies = { ...this.state.movies },
-        newMovies = Object.assign( currentMovies, additionalMovies );
-   
-    this.setState( { movies: newMovies } ); */
   }
 }
 
--- src/components/MovieList.js
+++ src/components/MovieList.js
@@ -7,28 +7,13 @@ class MovieList extends Component {
 		this.state = {
 			movies: [],
 		};
+
+		this.apiKey               = this.apiKey.bind( this );
+    	this.loadAdditionalMovies = this.loadAdditionalMovies.bind( this );
 	}
 
 	componentWillMount() {
-		fetch( 'https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=985e24fe0adef6dbea8259e606b71a37' )
-			.then( results => {
-				return results.json();
-			} )
-			.then( data => {
-				let movies = data.results.map( movie => {
-					const date = new Date( movie.release_date );
-
-					movie.release_date = new Intl.DateTimeFormat( 'en-US', {
-						'year': 'numeric'
-					} ).format( date );
-
-					return (
-						<Movie key={movie.id} meta={movie} />
-					)
-				} );
-
-				this.setState( { 'movies': movies } );
-			} );
+		this.loadAdditionalMovies();
 	}
 
 	render() {
@@ -38,6 +23,40 @@ class MovieList extends Component {
 			</div>
 		);
 	}
+
+	loadAdditionalMovies() {
+		/* var currentMovies = { ...this.state.movies },
+			newMovies = Object.assign( currentMovies, additionalMovies );
+	   
+		this.setState( { movies: newMovies } ); */
+		
+		fetch( 'https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=985e24fe0adef6dbea8259e606b71a37' )
+		.then( results => {
+				return results.json();
+			} )
+		.then( data => {
+			let movies = data.results.map( movie => {
+				const date = new Date( movie.release_date );
+
+				movie.release_date = new Intl.DateTimeFormat( 'en-US', {
+					'year': 'numeric'
+				} ).format( date );
+
+				return (
+					<Movie key={movie.id} meta={movie} />
+				)
+			} );
+
+			this.setState( { 'movies': movies } );
+	} );
+		
+	}
+	
+	  apiKey() {
+		  this.setState( {
+			  apiKey: '985e24fe0adef6dbea8259e606b71a37'
+		  } );
+	  }
 }
 
 export default MovieList;
