6a9d6c15c5d4dc8ca54b24f8501fc212c601db67
Mario Aguiar <mario@10up.com>
2018-02-23 11:55:36 -0600

    Set state with movie info

--- src/components/SingleMovie.js
+++ src/components/SingleMovie.js
@@ -1,26 +1,34 @@
 import React, { Component } from 'react';
 
 class SingleMovie extends Component {
+	constructor() {
+		super();
+
+		this.state = {
+			movie: {}
+		};
+	}
+
 	componentWillMount() {
 		const movieId = this.props.match.params.movieSlug,
 			  localStorageRef = localStorage.getItem( `mah-movie-${ movieId }` );
 
 		if ( localStorageRef && 'undefined' !== localStorageRef ) {
-			console.log( JSON.parse( localStorageRef ) );
+			this.setState( { 'movie': JSON.parse( localStorageRef ) } );
 		} else {
 			this.getMovieData( movieId );
 		}
 	}
 
-	getMovieData( movieId ) {
+	getMovieData = ( movieId ) => {
 		fetch( `https://api.themoviedb.org/3/movie/${ movieId }?api_key=985e24fe0adef6dbea8259e606b71a37` )
 		.then( results => {
 			return results.json();
 		} )
 		.then( data => {
-			console.log(data);
-
 			localStorage.setItem( `mah-movie-${ movieId }`, JSON.stringify( data ) );
+
+			this.setState( { 'movie': data } );
 		} );
 	}
 
