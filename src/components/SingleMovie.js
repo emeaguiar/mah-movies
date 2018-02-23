80bd35134eb63e9a3114a2052328d6ebcd9e8175
Mario Aguiar <mario@10up.com>
2018-02-23 11:52:17 -0600

    Save fetched data in local storage

--- src/components/SingleMovie.js
+++ src/components/SingleMovie.js
@@ -2,14 +2,25 @@ import React, { Component } from 'react';
 
 class SingleMovie extends Component {
 	componentWillMount() {
-		const movieId = this.props.match.params.movieSlug;
+		const movieId = this.props.match.params.movieSlug,
+			  localStorageRef = localStorage.getItem( `mah-movie-${ movieId }` );
 
+		if ( localStorageRef && 'undefined' !== localStorageRef ) {
+			console.log( JSON.parse( localStorageRef ) );
+		} else {
+			this.getMovieData( movieId );
+		}
+	}
+
+	getMovieData( movieId ) {
 		fetch( `https://api.themoviedb.org/3/movie/${ movieId }?api_key=985e24fe0adef6dbea8259e606b71a37` )
 		.then( results => {
 			return results.json();
 		} )
 		.then( data => {
 			console.log(data);
+
+			localStorage.setItem( `mah-movie-${ movieId }`, JSON.stringify( data ) );
 		} );
 	}
 
