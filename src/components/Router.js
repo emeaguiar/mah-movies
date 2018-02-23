412f5399350fc2fb84aa7a21690c1ca5c9770d2f
Mario Aguiar <mario@10up.com>
2018-02-23 10:12:17 -0600

    Add links to a single movie view

--- src/components/Router.js
+++ src/components/Router.js
@@ -1,12 +1,16 @@
 import React, { Component } from 'react';
 import { BrowserRouter, Route, Link } from 'react-router-dom';
 import App from './App';
+import SingleMovie from './SingleMovie';
 
 class Router extends Component {
 	render() {
 		return (
 			<BrowserRouter>
-				<Route exact path="/" component={ App } />
+				<div>
+					<Route exact path="/" component={ App } />
+					<Route path="/movie/:movieSlug" component={ SingleMovie } />
+				</div>
 			</BrowserRouter>
 		)
 	}
--- src/index.js
+++ src/index.js
@@ -3,5 +3,4 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import Router from './components/Router';
 
-
 ReactDOM.render(<Router />, document.getElementById('root'));
