ebecb1228011810625e840b031aa0c1232257547
Mario Aguiar <mario@10up.com>
2018-03-02 16:07:02 -0600

    Add styles for movie item

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -2,17 +2,20 @@ import React, { Component } from 'react';
 import Poster from './Poster';
 import { Link } from 'react-router-dom';
 
+import './styles/movie.css';
+
 class Movie extends Component {
 	render() {
 		return (
-			<div className="movie column">
-				<h2>
-					<Link to={ `/movie/${this.props.meta.id}` }>{ this.props.meta.title }</Link></h2>
+			<div className="movie-item column">
 				<div>
 					<Link to={ `/movie/${this.props.meta.id}` }>
 						<Poster path={ this.props.meta.poster_path } alt={ `Poster for ${ this.props.meta.title }` } />
 					</Link>
 				</div>
+				<h2 class="title">
+					<Link to={ `/movie/${this.props.meta.id}` }>{ this.props.meta.title }</Link>
+				</h2>
 				<p>({ this.props.meta.release_date })</p>
 				<p>{ this.props.meta.overview }</p>
 			</div>
